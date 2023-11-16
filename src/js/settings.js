f2e85eaf8e130f4f04fe9aba8da02919235f3608
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2023-11-16 23:47:24 +0100

    Add functionality for subpages

---
+++ src/js/settings.js
@@ -0,0 +1,38 @@
+export const select = {
+  templateOf: {
+    home: '#template-home',
+  },
+  containerOf: {
+    pages: '#pages',
+    home: '#home-wrapper',
+  },
+  nav: {
+    links: '.navigation a',
+  },
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    discover: 'discover',
+    search: 'search',
+    home: 'home',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+};
+
+export const templates = {
+  home: Handlebars.compile(document.querySelector(select.templateOf.home).innerHTML),
+};

f770a12db81f4646c7e3ba67c0160ed0ed62ed5b
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2023-11-22 11:14:10 +0100

    Render songs

--- src/js/components/HomeSongs.js
+++ src/js/components/HomeSongs.js
@@ -1,5 +1,4 @@
 import {templates, select} from '../settings.js';
-import utils from '../utils.js';
 
 class HomeSongs {
   constructor(id, data) {
@@ -14,14 +13,35 @@ class HomeSongs {
   render() {
     const thisHomeSongs = this;
 
-    const generatedHTML = templates.songs(thisHomeSongs.data);
-
-    thisHomeSongs.element = utils.createDOMFromHTML(generatedHTML);
-
+    // Clear the container before appending new songs
     const homeSongsContainer = document.querySelector(select.containerOf.songs);
+    homeSongsContainer.innerHTML = '';
 
-    homeSongsContainer.appendChild(thisHomeSongs.element);
+    // Iterate through each song and render its view
+    thisHomeSongs.data.songs.forEach(song => {
+      const songView = thisHomeSongs.createSongView(song);
+      homeSongsContainer.appendChild(songView);
+    });
+  }
+
+  createSongView(song) {
+    const thisHomeSongs = this;
+    // Create a new element for the song
+    const songElement = document.createElement('div');
+    songElement.classList.add('song-item');
+
+    // Populate the element with song details using the template
+    const generatedHTML = templates.songs({ songs: [song] });
+    songElement.innerHTML = generatedHTML;
+
+    // Initialize Green Audio Player for the current song
+    GreenAudioPlayer.init({
+      selector: `.player-${thisHomeSongs.id} .green-audio-player`,
+      stopOthersOnPlay: true,
+    });
+
+    return songElement;
   }
 }
 
-export default HomeSongs; 
+export default HomeSongs;
